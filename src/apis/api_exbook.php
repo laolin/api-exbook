782f4f5d0f626fa01bb82cb2c42581c96638a732
LaoLin <hi@laolin.com>
2017-05-20 16:36:02 +0800

    两个通用的API已改为 /feed /comment 移到 Api-core 库中。

--- src/apis/api_exbook.php
+++ src/apis/api_exbook.php
@@ -14,8 +14,6 @@
  *  
  */
 
-require_once  dirname( __FILE__ ) . '/class.EBCONFIG.php';
-require_once  dirname( __FILE__ ) . '/class.EXBOOK.php';
 
 class class_exbook{
   public static function main( $para1,$para2) {
@@ -34,283 +32,40 @@ class class_exbook{
     return API::data('Test passed.');
   }
   public static function config( ) {
-    return API::data(EBCONFIG::config());
+    $cfg=[];
+    $cfg['data_app']='exbook';
+    
+    $data_define=[];
+    $data_define[]=[
+      'column'=>'d1',
+      'name'=>'年级',
+      'type'=>'radio',
+      'data'=>[
+        'x0'=>'幼升小',
+        'x1'=>'一年级',
+        'x2'=>'二年级',
+        'x3'=>'三年级',
+        'x4'=>'四年级',
+        'x5'=>'五年级/小升初',
+        'x6'=>'初中以上'
+      ]
+    ];
+    $data_define[]=[
+      'column'=>'d2',
+      'name'=>'科目',
+      'type'=>'radio',
+      'data'=>[
+        'yu'=>'语文',
+        'shu'=>'数学',
+        'ying'=>'英语',
+        'other'=>'其他'
+      ]
+    ];
+
+    $cfg['data_define']=$data_define;
+    return API::data($cfg);
   }  
   
 
-  /**
-   *  API:
-   *    /exbook/draft_create
-   *  输入: 
-   *    uid
-   *  
-   *  返回:
-   *    各字段
-   *  
-   *  注：为简化系统， 默认规定只能有一个 草稿
-   *  所以当 uid 用户已有草稿时，此API是返回原有的草稿
-   */
-  public static function draft_create( ) {
-    $r=self::userVerify();
-    if(!$r)
-      return API::msg(202001,'error userVerify');
-    $uid=API::INP('uid');    
-    $r=EXBOOK::draft_get_by_uid($uid);
-    if(!$r) {
-      $r=EXBOOK::draft_create($uid);
-    }
-    return API::data($r);
-  }
-  
-  /**
-   *  API:
-   *    /exbook/draft_get
-   *  输入: 
-   *    uid
-   *    fid 
-   *  
-   *  返回:
-   *    各字段
-   */
-  public static function draft_get( ) {
-    $r=self::userVerify();
-    if(!$r)
-      return API::msg(202001,'error userVerify');
-    
-    $uid=API::INP('uid');    
-    $fid=API::INP('fid');
-    $r=EXBOOK::feed_get($uid,$fid,'draft');
-    //if(API::is_error($r)){
-    //  return $r;
-    //}
-    return $r;
-  }
-
-  /**
-   *  API:
-   *    /exbook/draft_get_by_user
-   *    自动按uid查找一个draft
-   *  输入: 
-   *    uid
-   *  
-   *  返回:
-   *    各字段
-   */
-  public static function draft_get_by_user( ) {
-    $r=self::userVerify();
-    if(!$r)
-      return API::msg(202001,'error userVerify');
-    
-    $uid=API::INP('uid');    
-    $r=EXBOOK::draft_get_by_uid($uid);
-    if(!$r) {
-      $r=EXBOOK::draft_create($uid);
-    }
-    return API::data($r);
-  }
-
-  /**
-   *  API:
-   *    /exbook/draft_get_deleted
-   *    自动按uid查找一个已删除的draft
-   *  输入: 
-   *    uid
-   *  
-   *  返回:
-   *    各字段
-   */
-  public static function draft_get_deleted( ) {
-    $r=self::userVerify();
-    if(!$r)
-      return API::msg(202001,'error userVerify');
-    
-    $uid=API::INP('uid');    
-    $r=EXBOOK::draft_get_deleted_by_uid($uid);
-    return API::data($r);
-  }
-
-  /**
-   *  API:
-   *    /exbook/draft_delete
-   *  输入: 
-   *    uid
-   *    fid
-   *  
-   *  返回:
-   *    1 or 0 表示有无更新
-   */
-  public static function draft_delete( ) {
-    $r=self::userVerify();
-    if(!$r)
-      return API::msg(202001,'error userVerify');
-    
-    $uid=API::INP('uid');    
-    $fid=API::INP('fid');
-    //要确保fid是对应一个存在的数据
-    $r=EXBOOK::feed_get($uid,$fid,'draft');
-    if(API::is_error($r)){
-      return $r;
-    }
-    $r=EXBOOK::feed_delete( $fid );
-    return API::data($r);
-  }
-
-  /**
-   *  API:
-   *    /exbook/draft_undelete
-   *  输入: 
-   *    uid
-   *    fid
-   *  
-   *  返回:
-   *    1 or 0 表示有无更新
-   */
-  public static function draft_undelete( ) {
-    $r=self::userVerify();
-    if(!$r)
-      return API::msg(202001,'error userVerify');
-    
-    $uid=API::INP('uid');    
-    $fid=API::INP('fid');
-    //要确保fid是对应一个存在的数据
-    $r=EXBOOK::feed_get($uid,$fid,'draft',true ); //true 表示可以是已删除的
-    if(API::is_error($r)){
-      return $r;
-    }
-    
-    if(!$r['data']['del'])
-      return API::msg(202201,"fid $fid was normal");
-    $r=EXBOOK::feed_undelete( $fid );
-    return API::data($r);
-  }
-  
-  /**
-   *  API:
-   *    /exbook/draft_update
-   *  输入: 
-   *    uid
-   *    fid
-   *    其他要更新的字段 : EXBOOK::data_all()函数
-   *  
-   *  返回:
-   *    1 or 0 表示有无更新
-   *  
-   */
-  public static function draft_update( ) {
-    $r=self::userVerify();
-    if(!$r)
-      return API::msg(202001,'error userVerify');
-    
-    $uid=API::INP('uid');
-    $fid=API::INP('fid');
-    
-    //要确保fid是对应一个存在的数据
-    $r=EXBOOK::feed_get($uid,$fid,'draft');
-    if(API::is_error($r)){
-      return $r;
-    }
-
-    $data=EXBOOK::data_all();
-    $r=EXBOOK::feed_update($fid,$data);
-    return API::data($r);
-  }
-
-  /**
-   *  API:
-   *    /exbook/draft_publish
-   *  输入: 
-   *    uid
-   *    fid
-   *  
-   *  返回:
-   *    1 or 0 表示有无更新
-   */
-  public static function draft_publish( ) {
-    $r=self::userVerify();
-    if(!$r)
-      return API::msg(202001,'error userVerify');
-
-    $uid=API::INP('uid');    
-    $fid=API::INP('fid');
-    //要确保fid是对应一个存在的数据
-    $r=EXBOOK::feed_get($uid,$fid,'draft');
-    if(API::is_error($r)){
-      return $r;
-    }
-    $err=EXBOOK::feed_validate($r['data']);
-    if($err){
-      return API::msg(202002,$err);
-    }
-    $r=EXBOOK::draft_publish($fid );
-    return API::data($r);
-  }
-  
-  //fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-  
-  /**
-   *  API:
-   *    /exbook/feed_get
-   *  输入: 
-   *    uid
-   *    fid 
-   *  
-   *  返回:
-   *    各字段
-   */
-  public static function feed_get( ) {
-    $r=self::userVerify();
-    if(!$r)
-      return API::msg(202001,'error userVerify');
-    
-    $uid=API::INP('uid');    
-    $fid=API::INP('fid');
-    $r=EXBOOK::feed_get($uid,$fid,'publish');
-    return $r;
-  }
- 
-  /**
-   *  API:
-   *    /exbook/feed_list
-   *  输入: 
-   *    uid
-   *  
-   *  返回:
-   *    各字段
-   */
-  public static function feed_list( ) {
-    $r=self::userVerify();
-    if(!$r)
-      return API::msg(202001,'error userVerify');
-    
-    $uid=API::INP('uid');    
-    $r=EXBOOK::feed_list($uid,'publish');
-    return $r;
-  }
-  /**
-   *  API:
-   *    /exbook/feed_delete
-   *  输入: 
-   *    uid
-   *    fid
-   *  
-   *  返回:
-   *    1 or 0 表示有无更新
-   */
-  public static function feed_delete( ) {
-    $r=self::userVerify();
-    if(!$r)
-      return API::msg(202001,'error userVerify');
-    
-    $uid=API::INP('uid');    
-    $fid=API::INP('fid');
-    //要确保fid是对应一个存在的数据
-    $r=EXBOOK::feed_get($uid,$fid,'publish');
-    if(API::is_error($r)){
-      return $r;
-    }
-    $r=EXBOOK::feed_delete( $fid );
-    return API::data($r);
-  }
- 
 }
 
